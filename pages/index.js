84c3a9a11c0bc5d12349e11f46848b1e533503d0
Augusto Gonçalves <33488726+gutogoncalves@users.noreply.github.com>
2024-12-13 21:25:18 +0000

    atualizando os arquivos alterados pelo `Prittier`

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 function Home() {
-
-
-    return <div>
-                <h1>
-                    <center>
-                        Eu consegui minha lindinha!
-                    </center>
-                </h1>
-                <p>Lara, essa é uma pequena demonstração de todo o meu amor e gratidão por você.</p>
-                <p>Consegui completar um desafio do curso e pra mim vai ficar como o marco de início de mudança das nossas vidas.</p>
-                <p>Te Amo s2</p>
-                <p>Gos!</p>
-            </div>
+  return (
+    <div>
+      <h1>
+        <center>Eu consegui minha lindinha!</center>
+      </h1>
+      <p>
+        Lara, essa é uma pequena demonstração de todo o meu amor e gratidão por
+        você.
+      </p>
+      <p>
+        Consegui completar um desafio do curso e pra mim vai ficar como o marco
+        de início de mudança das nossas vidas.
+      </p>
+      <p>Te Amo s2</p>
+      <p>Gos!</p>
+    </div>
+  );
 }
 
 export default Home;
